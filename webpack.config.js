4ba9d10d5f235aa5b5b657ab1f86da2ebfe48a70
rafallyczek <rafal.lyczek@gmail.com>
2023-04-15 20:53:09 +0200

    Add MiniCssExtract and HtmlWebpack
    Replaced style-loader with MiniCssExtract plugin and added
    HtmlWebpack to generate index file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [ 
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
           filename: "img/[hash][ext][query]",
         },
