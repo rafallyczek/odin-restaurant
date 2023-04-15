f20a8fa3b311f7580eb4a8988abea085a163a1dd
rafallyczek <rafal.lyczek@gmail.com>
2023-04-15 20:18:05 +0200

    Set up rules for css and image loading
    Added rules for style and image loading to webpack
    configuration. Also created basic page layout.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("test");
+import "./style.css";
+
+function content(){
+    const content = document.querySelector(".content");
+
+    const header = document.createElement("header");
+    const main = document.createElement("main");
+    const footer = document.createElement("footer");
+
+    content.appendChild(header);
+    content.appendChild(main);
+    content.appendChild(footer);
+}
+
+content();
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,24 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "img/[hash][ext][query]",
+        },
+      },
+    ],
   },
 };
