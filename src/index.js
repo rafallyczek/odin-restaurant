4e0cbd5b272f538e9c6a0b0393f93a4ef33f6d89
rafallyczek <rafal.lyczek@gmail.com>
2023-04-17 10:16:10 +0200

    Adjust page layout and add footer content
    Made small changes to page layout and filled footer with content.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,77 @@
 import "./style.css";
 
-function content(){
-    const content = document.querySelector(".content");
+function footer() {
+  const footer = document.createElement("footer");
+  footer.classList.add("bgc-dark");
+  footer.classList.add("text-center");
 
-    const header = document.createElement("header");
-    const main = document.createElement("main");
-    const footer = document.createElement("footer");
+  const author = document.createElement("p");
+  let text = document.createTextNode("Page made by ");
+  let link = document.createElement("a");
+  link.textContent = "rafallyczek";
+  link.href = "https://github.com/rafallyczek";
+  link.target = "_blank";
 
-    content.appendChild(header);
-    content.appendChild(main);
-    content.appendChild(footer);
+  author.appendChild(text);
+  author.appendChild(link);
+
+  text = document.createTextNode(" while working with ");
+  link = document.createElement("a");
+  link.textContent = "The Odin Project";
+  link.href = "https://www.theodinproject.com/";
+  link.target = "_blank";
+
+  author.appendChild(text);
+  author.appendChild(link);
+
+  const photo = document.createElement("p");
+  link = document.createElement("a");
+  link.textContent = "Photo";
+  link.href = "https://unsplash.com/photos/MQUqbmszGGM";
+  link.target = "_blank";
+  text = document.createTextNode(" by ");
+
+  photo.appendChild(link);
+  photo.appendChild(text);
+
+  link = document.createElement("a");
+  link.textContent = "Ivan Torres";
+  link.href = "https://unsplash.com/@iavnt";
+  link.target = "_blank";
+  text = document.createTextNode(" at ");
+
+  photo.appendChild(link);
+  photo.appendChild(text);
+
+  link = document.createElement("a");
+  link.textContent = "Unsplash";
+  link.href = "https://unsplash.com/";
+  link.target = "_blank";
+
+  photo.appendChild(link);
+
+  footer.appendChild(photo);
+  footer.appendChild(author);
+
+  return footer;
+}
+
+function content() {
+  const content = document.querySelector(".content");
+
+  const header = document.createElement("header");
+  header.classList.add("bgc-dark");
+
+  const mainContainer = document.createElement("div");
+  mainContainer.classList.add("main");
+
+  const main = document.createElement("main");
+
+  mainContainer.appendChild(main);
+  mainContainer.appendChild(footer());
+
+  content.appendChild(header);
+  content.appendChild(mainContainer);
 }
 
 content();
